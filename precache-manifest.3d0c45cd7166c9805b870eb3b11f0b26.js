222edb18a8030d8aa60863e9608c0b3dcff55bee
Gauri Bhand <htcgauribhand@gmail.com>
2022-02-07 21:19:00 +0530

    Updates

---
+++ precache-manifest.3d0c45cd7166c9805b870eb3b11f0b26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4821c02975a1d1fb2dfd0e11fb49406e",
+    "url": "/Contact-Manager-with-Firebase/index.html"
+  },
+  {
+    "revision": "1a6e51c8b9df7741ffc3",
+    "url": "/Contact-Manager-with-Firebase/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "35fef61b51a08664ec91",
+    "url": "/Contact-Manager-with-Firebase/static/js/2.a8f27175.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/Contact-Manager-with-Firebase/static/js/2.a8f27175.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a6e51c8b9df7741ffc3",
+    "url": "/Contact-Manager-with-Firebase/static/js/main.c1f3c87d.chunk.js"
+  },
+  {
+    "revision": "665a24df26edbcdd8870",
+    "url": "/Contact-Manager-with-Firebase/static/js/runtime-main.8efa496f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Contact-Manager-with-Firebase/static/media/logo.5d5d9eef.svg"
+  }
+]);
